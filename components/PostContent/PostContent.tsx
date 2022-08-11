0465be9dcc21194fd20bc6db99ab5a8c1dc8ef76
ahmedbhameed <contact@ahmedhameed.dev>
2022-08-11 23:53:57 +0200

    Revert back to manul meta tag

--- components/PostContent/PostContent.tsx
+++ components/PostContent/PostContent.tsx
@@ -1,12 +1,11 @@
 import {get} from 'lodash';
-import {NextSeo} from 'next-seo';
 import React from 'react';
 import {DOMAIN} from '../../config/environments';
 import ROUTES from '../../config/routes';
 import {Post} from '../../graphql/generated/graphql';
 import slugToTitle from '../../utils/slugToTitle';
 import MDPreviewClient from '../MDPreview/MDPreviewClient';
-// import MetaTags from '../MetaTags/MetaTags';
+import MetaTags from '../MetaTags/MetaTags';
 
 interface PostContentProps {
   post?: Post;
@@ -17,31 +16,45 @@ const PostContent: React.FC<PostContentProps> = ({post}) => {
   const slug = slugToTitle(post.slug);
   const twitterDescription = get(post, 'postContents.0.contentPreview');
 
-  const SEO = {
-    title: `${slugToTitle(post.slug)} | Rocket Dev`,
-    description: twitterDescription,
-    facebook: {
-      appId: post.id,
-    },
-    openGraph: {
-      type: 'website',
-      locale: 'en_IE',
-      images: image,
-      url: `${DOMAIN}/${ROUTES.post.path}/${post.slug}/${post.nanoId}`,
-      title: slugToTitle(post.slug),
-      site_name: 'Rocket Dev',
-    },
+  // const SEO = {
+  //   title: `${slugToTitle(post.slug)} | Rocket Dev`,
+  //   description: twitterDescription,
+  //   facebook: {
+  //     appId: post.id,
+  //   },
+  //   openGraph: {
+  //     type: 'website',
+  //     locale: 'en_IE',
+  //     images: image,
+  //     url: `${DOMAIN}/${ROUTES.post.path}/${post.slug}/${post.nanoId}`,
+  //     title: slugToTitle(post.slug),
+  //     site_name: 'Rocket Dev',
+  //   },
 
-    twitter: {
-      handle: '@Ahmed_B_HAMEED',
-      site: '@Ahmed_B_HAMEED',
-      cardType: 'summary_large_image',
-    },
-  };
-
+  //   twitter: {
+  //     handle: '@Ahmed_B_HAMEED',
+  //     site: '@Ahmed_B_HAMEED',
+  //     cardType: 'summary_large_image',
+  //   },
+  //   images: [
+  //     {
+  //       url: image,
+  //       width: 800,
+  //       height: 600,
+  //       alt: slug,
+  //     },
+  //   ],
+  // };
   return (
     <div className="relative py-24 overflow-hidden">
-      <NextSeo {...SEO} />
+      <MetaTags
+        articleBy="@Ahmed_B_HAMEED"
+        articleId={post.id}
+        articleUrl={`${DOMAIN}/${ROUTES.post.path}/${post.slug}/${post.nanoId}`}
+        description={twitterDescription}
+        imageUrl={image}
+        title={slug}
+      />
       <div className="relative px-4 sm:px-6 lg:px-8">
         <div className="text-lg max-w-prose mx-auto">
           <h1>
