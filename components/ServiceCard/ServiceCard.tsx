1be51055d63f2f2c67eaec4feca9a6e90ded508b
Ahmed HAMEED <ahmed@pop-os.localdomain>
2022-09-17 22:20:59 +0200

    Enhance course links

---
+++ components/ServiceCard/ServiceCard.tsx
@@ -0,0 +1,167 @@
+import {get} from 'lodash';
+import Image from 'next/image';
+import Link from 'next/link';
+import {useRouter} from 'next/router';
+import React from 'react';
+import ROUTES from '../../config/routes';
+import {Post} from '../../graphql/generated/graphql';
+import theme from '../../styles/theme';
+import clsx from '../../utils/clsx';
+import {getPathWithoutStartSlash} from '../../utils/getPathWithoutLastSlash';
+import slugToTitle from '../../utils/slugToTitle';
+import titleToSlug from '../../utils/titleToSlug';
+import ReadingTime from '../ReadingTime/ReadingTime';
+import ReadMore from '../ReadMore/ReadMore';
+
+interface ServiceCardProps {
+  post: Post;
+  index: number;
+}
+
+export const ServiceCard = ({post, index}: ServiceCardProps) => {
+  const router = useRouter();
+  const imageUrl =
+    get(post, 'postContents.0.postImage') ||
+    'https://minio.rocketdev.dev/blog/courses/placeholders/empty_preview.jpg';
+
+  const postLink = `${getPathWithoutStartSlash(router.asPath)}${
+    ROUTES.post.path
+  }/${titleToSlug(post.slug)}/${post.nanoId}`;
+
+  return (
+    <Link href={postLink}>
+      <a>
+        <div
+          className={clsx(
+            theme.bgSecondary,
+            'title-hover',
+            'service-card',
+            'flex',
+            'gap-5',
+            'h-64',
+            'my-2',
+            'overflow-hidden',
+            'flex-nowrap',
+            'border',
+            'rounded-lg',
+            'border-gray-500',
+            'cursor-pointer',
+            'hover:border-indigo-500'
+            // checked ? 'border-transparent' : 'border-gray-300',
+            // active ? 'border-indigo-500 ring-2 ring-indigo-500' : '',
+          )}
+        >
+          <div className={clsx('w-1/3', 'relative')}>
+            <span
+              className={clsx(
+                'absolute',
+                'bottom-2',
+                'left-4',
+                'z-10',
+                'font-bold',
+                'text-gray-50'
+              )}
+            >
+              {index.toString().padStart(2, '0')}
+            </span>
+
+            <div
+              className={clsx('relative', 'h-full')}
+              style={{
+                backgroundImage: `linear-gradient(to bottom, rgba(242, 65, 37, 0.2), rgba(242, 65, 37, 0.6))`,
+                clipPath: 'polygon(0 0, 100% 0%, 90% 100%, 0% 100%)',
+              }}
+            >
+              <Image
+                className={clsx('opacity-70', 'bg-cover')}
+                src={imageUrl}
+                alt="post"
+                layout="fill"
+                crossOrigin="anonymous"
+              />
+            </div>
+          </div>
+
+          <div
+            className={clsx(
+              'w-2/3',
+              'p-4',
+              'flex',
+              'flex-col',
+              'justify-between'
+            )}
+          >
+            <div>
+              <div className={clsx('flex', 'justify-between')}>
+                <small
+                  className={clsx(
+                    'prose',
+                    'sm:prose-sm',
+                    'italic',
+                    'text-blue-500',
+                    'font-medium',
+                    'title-hover--title'
+                  )}
+                >
+                  {post.groupName}
+                </small>
+
+                <span
+                  className={clsx(
+                    'font-medium',
+                    'title-hover--title',
+                    theme.text
+                  )}
+                >
+                  <ReadingTime
+                    readingTime={get(post, 'postContents.0.readingTime', '')}
+                  />
+                </span>
+              </div>
+
+              <h6
+                className={clsx(
+                  theme.text,
+                  'prose',
+                  'lg:prose-2xl',
+                  'font-medium',
+                  'title-hover--title'
+                )}
+              >
+                {slugToTitle(post.slug)}
+              </h6>
+
+              <p
+                className={clsx(
+                  'block',
+                  'sm:inline',
+                  'dark:text-gray-300',
+                  'text-gray-500'
+                )}
+              >
+                {post.postContents[0].contentPreview}
+              </p>
+
+              {/* <ul role="list" className={clsx('ml-6', 'mt-4')}>
+                    {post.postContents[0].headLines.map((headLine) => (
+                        <li key={headLine} className={clsx('my-1')}>
+                        <div className="focus-within:ring-2 focus-within:ring-indigo-500">
+                            <h3
+                            className={clsx('text-sm', 'font-semibold', 'text-gray-400')}
+                            >
+                            <span>{startCase(toLower(headLine.replace(/#/g, '')))}</span>
+                            </h3>
+                        </div>
+                        </li>
+                    ))}
+                </ul> */}
+            </div>
+            <div className={clsx('flex')}>
+              <ReadMore />
+            </div>
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
+};
--- pages/courses/[slug]/[courseId]/contents/index.tsx
+++ pages/courses/[slug]/[courseId]/contents/index.tsx
@@ -1,17 +1,13 @@
 import {ApolloQueryResult} from '@apollo/client';
-import {RadioGroup} from '@headlessui/react';
-import {get, startCase, toLower} from 'lodash';
+import {get} from 'lodash';
 import {GetServerSideProps} from 'next';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
-import {useRouter} from 'next/router';
-import React, {useCallback, useState} from 'react';
+import React from 'react';
 import Col from '../../../../../components/Col/Col';
 import BoldLabel from '../../../../../components/Label/BoldLabel';
 import Layout from '../../../../../components/Layout';
-import ReadingTime from '../../../../../components/ReadingTime/ReadingTime';
 import Row from '../../../../../components/Row/Row';
-import ROUTES from '../../../../../config/routes';
-
+import {ServiceCard} from '../../../../../components/ServiceCard/ServiceCard';
 import {
   GetCourseContentsQuery,
   GetCourseContentsQueryVariables,
@@ -19,12 +15,8 @@ import {
   Post,
 } from '../../../../../graphql/generated/graphql';
 import GET_COURSE_CONTENTS_QUERY from '../../../../../graphql/GET_COURSE_CONTENTS_QUERY.gql';
-import theme from '../../../../../styles/theme';
 import apolloClient from '../../../../../utils/apolloClient';
-import clsx from '../../../../../utils/clsx';
-import {getPathWithoutStartSlash} from '../../../../../utils/getPathWithoutLastSlash';
 import slugToTitle from '../../../../../utils/slugToTitle';
-import titleToSlug from '../../../../../utils/titleToSlug';
 
 interface CourseContent {
   courseContents: Post[];
@@ -32,21 +24,6 @@ interface CourseContent {
 }
 
 const CourseContent = ({courseContents, courseSlug}: CourseContent) => {
-  const router = useRouter();
-  const [selectedPostUri, setSelectedPostUri] = useState('');
-
-  const onSelectedPost = useCallback(
-    (postUri: string) => {
-      setSelectedPostUri(postUri);
-      router.push(
-        `${getPathWithoutStartSlash(router.asPath)}${
-          ROUTES.post.path
-        }/${postUri}`
-      );
-    },
-    [router]
-  );
-
   return (
     <Layout>
       <Row gutter={[8, 8]} md={2}>
@@ -59,118 +36,9 @@ const CourseContent = ({courseContents, courseSlug}: CourseContent) => {
         </Col>
       </Row>
 
-      <RadioGroup value={selectedPostUri} onChange={onSelectedPost}>
-        <RadioGroup.Label className="sr-only">Server size</RadioGroup.Label>
-        <div className="space-y-1">
-          {courseContents.map((post, index) => (
-            <RadioGroup.Option
-              key={post.nanoId}
-              value={`${titleToSlug(post.slug)}/${post.nanoId}`}
-              className={({checked, active}) =>
-                clsx(
-                  checked ? 'border-transparent' : 'border-gray-300',
-                  active ? 'border-indigo-500 ring-2 ring-indigo-500' : '',
-                  theme.bgSecondary,
-                  'relative',
-                  'block',
-                  'border',
-                  'rounded-lg',
-                  'shadow-sm',
-                  'px-6',
-                  'py-4',
-                  'cursor-pointer',
-                  'sm:flex',
-                  'sm:justify-between',
-                  'focus:outline-none',
-                  'title-hover'
-                )
-              }
-            >
-              {({active, checked}) => (
-                <>
-                  <span className="flex items-center">
-                    <span className="text-sm flex flex-col">
-                      <RadioGroup.Label
-                        as="span"
-                        className={clsx(
-                          'text-lg',
-                          'font-medium',
-                          'mb-2',
-                          'title-hover--title'
-                        )}
-                      >
-                        {post.groupName}&nbsp;{'=>'}&nbsp;&nbsp;&nbsp;&nbsp;
-                        {index.toString().padStart(3, '0')}-
-                        {slugToTitle(post.slug)}
-                      </RadioGroup.Label>
-                      <RadioGroup.Description
-                        as="span"
-                        className={clsx(theme.text)}
-                      >
-                        <span className="block sm:inline">
-                          {post.postContents[0].contentPreview}
-                        </span>
-
-                        <div className="flow-root">
-                          <ul role="list" className={clsx('ml-6', 'mt-4')}>
-                            {post.postContents[0].headLines.map((headLine) => (
-                              <li key={headLine} className={clsx('my-1')}>
-                                <div className="focus-within:ring-2 focus-within:ring-indigo-500">
-                                  <h3
-                                    className={clsx(
-                                      'text-sm',
-                                      'font-semibold',
-                                      'text-gray-400'
-                                    )}
-                                  >
-                                    <span>
-                                      {startCase(
-                                        toLower(headLine.replace(/#/g, ''))
-                                      )}
-                                    </span>
-                                  </h3>
-                                </div>
-                              </li>
-                            ))}
-                          </ul>
-                        </div>
-                      </RadioGroup.Description>
-                    </span>
-                  </span>
-                  <RadioGroup.Description
-                    as="span"
-                    className="mt-2 flex text-sm sm:mt-0 sm:flex-col sm:ml-4 sm:text-right"
-                  >
-                    <span
-                      className={clsx(
-                        'font-medium',
-                        'title-hover--title',
-                        theme.text
-                      )}
-                    >
-                      <ReadingTime
-                        readingTime={get(
-                          post,
-                          'postContents.0.readingTime',
-                          ''
-                        )}
-                      />
-                    </span>
-                  </RadioGroup.Description>
-                  <span
-                    className={clsx(
-                      active ? 'border' : 'border-2',
-                      checked ? 'border-indigo-500' : 'border-transparent',
-                      'absolute -inset-px rounded-lg pointer-events-none'
-                    )}
-                    aria-hidden="true"
-                  />
-                </>
-              )}
-            </RadioGroup.Option>
-          ))}
-        </div>
-      </RadioGroup>
+      {courseContents.map((post, index) => (
+        <ServiceCard key={post.nanoId} post={post} index={index} />
+      ))}
     </Layout>
   );
 };
